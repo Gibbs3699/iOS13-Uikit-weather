63a45d7b9908c437eed1f06b3e57b01d1311a687
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-02-20 11:26:43 +0700

    Adding user interface to storyboard

---
+++ iOS13-Uikit-weather/Controller/WeatherViewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS13-Uikit-weather
+//
+//  Created by admin on 19/2/2565 BE.
+//
+
+import UIKit
+
+class WeatherViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

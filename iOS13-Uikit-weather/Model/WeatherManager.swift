9da528e87b5a1ab18efda4e9e1dbe99fb701d2fc
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-02-26 18:44:36 +0700

    Using URLSession for Networking

--- iOS13-Uikit-weather/Model/WeatherManager.swift
+++ iOS13-Uikit-weather/Model/WeatherManager.swift
@@ -12,6 +12,34 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
         print(urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. create URL
+        if let url = URL(string: urlString) { //prevent nil
+            //2. create urlSesstion
+            let session = URLSession(configuration: .default) //just like a web browser
+            
+            //3.give a session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            
+            //4.start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return 
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
+        
     }
 }
